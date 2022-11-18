95caf9b22ca123d0a043e751c2587cd630727b32
Nano <nano.anderson@gmail.com>
2022-11-18 08:11:57 -1000

    Doing this in github fork instead of copying locally for dev work was dumb

--- Sources/Dynamic/Invocation.swift
+++ Sources/Dynamic/Invocation.swift
@@ -143,7 +143,7 @@ public class Invocation: Loggable {
         }
     }
 
-    func invoke() {
+    public func invoke() {
         guard let invocation = invocation, !isInvoked else { return }
 
         log("Invoking...")
